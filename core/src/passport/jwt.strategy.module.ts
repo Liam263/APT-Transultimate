397d37070c8c119ede80019f8b86a67a7a333154
Liam263 <nguyenthanhlam091@gmail.com>
2024-04-16 12:42:25 +1000

    add jwt.strategy.module.ts

---
+++ core/src/passport/jwt.strategy.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy'; 
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
+  providers: [
+    JwtStrategy,
+    Strategy,
+  ],
+  exports: [JwtStrategy], 
+})
+export class JwtStrategyModule {}
